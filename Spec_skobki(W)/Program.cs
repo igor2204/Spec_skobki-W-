d9e62cf80e7743a2313bb81c35054c7b003ab304
Цомкалов Игорь <igorcomkalov@gmail.com>
2024-05-12 23:29:35 +0300

    Добавьте файлы проекта.

---
+++ Spec_skobki(W)/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку:");
+        string inputString = Console.ReadLine();
+
+        string outputString = HighlightRepeatedWords(inputString);
+
+        Console.WriteLine(outputString);
+        Console.ReadLine();
+    }
+
+    static string HighlightRepeatedWords(string input)
+    {
+        string[] words = input.Split(' ');
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+        foreach (string word in words)
+        {
+            if (wordCount.ContainsKey(word))
+            {
+                wordCount[word]++;
+            }
+            else
+            {
+                wordCount[word] = 1;
+            }
+        }
+
+        string result = "";
+        foreach (string word in words)
+        {
+            if (wordCount[word] > 1)
+            {
+                result += $"<strong>{word}</strong> ";
+            }
+            else
+            {
+                result += $"{word} ";
+            }
+        }
+
+        return result.TrimEnd();
+    }
+}
